860a02464cb1491d758e42a4d04c7cd89d470bd5
stevekutz <stkutz@gmail.com>
2021-04-05 21:09:12 -0500

    added basic express service

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
- const express = require('express');
+const express = require('express');
+const {randomBytes} = require('crypto');
+const bodyParser = require('body-parser');
 
- const app = express();
+const app = express();
+app.use(bodyParser.json());
 
- app.get('/posts , (req, res) => {}');
 
- app.post('/posts', (req, res) => {});
+const posts = {};
+
+ app.get('/posts' , (req, res) => {
+    res.send(post);
+ });
+
+ app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
+
+    psots[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+ 
+ });
 
  app.listen(4000, () => {
     console.log("listening on port 4000");
